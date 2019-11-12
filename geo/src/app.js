b0ae23e33f38f3a373cf5da1d952422e44ea729f
Paul Diaz Navarrete <paulrrdiaz@gmail.com>
2019-11-11 21:06:28 -0500

    Add container/presentational component

--- geo/src/app.js
+++ geo/src/app.js
@@ -13,23 +13,46 @@ const Presentational = props => {
 };
 
 class Container extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      lng: null,
+      isFetching: false
+    };
+
+    console.log("constructor");
+    this.setPosition = this.setPosition.bind(this);
+  }
+
   componentDidMount() {
+    this.setState({ isFetching: true });
     console.log("mounted...");
     if (navigator.geolocation) {
-      console.log(navigator.geolocation);
-      navigator.geolocation.getCurrentPosition(function(coords) {
-        console.log("---");
-        console.log(coords);
-      });
+      navigator.geolocation.getCurrentPosition(this.setPosition);
     }
   }
 
+  setPosition({ coords: { latitude, longitude } }) {
+    setTimeout(() => {
+      this.setState({
+        lat: latitude,
+        lng: longitude,
+        isFetching: false
+      });
+    }, 2000);
+  }
+
   render() {
+    const { isFetching } = this.state;
     console.log("rendering...");
+    console.log(this.state);
 
     return (
       <div className="container">
         <h2>Container</h2>
+        {isFetching ? "Loading..." : <Presentational {...this.state} />}
       </div>
     );
   }
