9a1ff8ad3e15e9a11d155bf861f36149b71786f9
Paul Diaz Navarrete <paulrrdiaz@gmail.com>
2019-11-18 21:30:35 -0500

    Add redux basics

---
+++ redux-basics/src/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import {
+  compose,
+  createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux'
+
+const add10 = x => x + 10
+const multiply5 = x => x * 5
+const minus3 = x => x - 3
+
+const minus3multiply5add10 = compose(
+  add10, 
+  multiply5, 
+  minus3
+)
+// console.log(minus3multiply5add10(4));
+const initialStateMessage = {
+  messages: [],
+}
+
+const initialStateCounter = {
+  current: 0,
+}
+
+const messageReducer = (state = initialStateMessage, action) => {
+  return state;
+}
+
+const counterReducer = (state = initialStateCounter, action) => {
+  if (action.type === "ADD") {
+    return {
+      current: state.current + action.payload.amount
+    }
+  }
+  
+  return state
+}
+
+const reducers = combineReducers({
+  counter: counterReducer,
+  message: messageReducer
+})
+
+const logger = (store) => (next) => (action) => {
+  const prevState = store.getState()
+  const returnValue = next(action)
+  const nextState = store.getState()
+  console.log(`action ${action.type} from middleware`);
+  console.log('%c prevState', 'color: #4cd137', JSON.stringify(prevState, null, 2));
+  console.log('%c nextState', 'color: #e84118', JSON.stringify(nextState, null, 2));
+  
+  return returnValue
+}
+
+const store = createStore(
+  reducers,
+  applyMiddleware(logger)
+)
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState(), "from subscribe");
+})
+
+const action = {
+  type: 'ADD',
+  payload: {
+    amount: 5,
+  }
+}
+
+const actionCreator = amount => ({
+  type: 'ADD',
+  payload: {
+    amount,
+  }
+})
+
+store.dispatch(action)
+store.dispatch(actionCreator(10))
+store.dispatch(actionCreator(-25))
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
