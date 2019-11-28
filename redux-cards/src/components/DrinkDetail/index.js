ef833ba614cefe0bdc1aeca5ca8af5ca3279b57a
Paul Diaz Navarrete <paulrrdiaz@gmail.com>
2019-11-27 21:22:46 -0500

    Redux cards part 7 reach/router

--- redux-cards/src/components/DrinkDetail/index.js
+++ redux-cards/src/components/DrinkDetail/index.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const DrinkDetail = styled('div')`
+  text-align: center;
+
+  h2 { 
+    margin-bottom: 10px;
+  }
+
+  picture {
+    display: block;
+    margin: 0 auto;
+    width: 50%;
+  }
+
+  img {
+    max-width: 100%;
+    width: 100%;
+  }
+`
 
 export default (props) => {
-  console.log(props);
-
-  const { strDrink, strIngredient1, strIngredient2, strDrinkThumb } = props.drink
+  const { strDrink = "", strIngredient1 = "", strIngredient2 = "", strDrinkThumb = "" } = props.drink || {}
 
   return (
-    <div>
+    <DrinkDetail>
       <h2>
         {strDrink} {strIngredient1} {strIngredient2}
       </h2>
       <picture>
         <img src={strDrinkThumb} alt="" />
       </picture>
-    </div>
+    </DrinkDetail>
   )
 }
